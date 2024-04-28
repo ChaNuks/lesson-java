ec705616587d6c60060955d74813fa997aee74cf
ChaNuks <cksdnrtls33@gmail.com>
2024-04-28 14:37:36 +0900

    1회

--- app/src/main/java/lesson/java/variable/Main.java
+++ app/src/main/java/lesson/java/variable/Main.java
@@ -6,9 +6,15 @@ public class Main {
         String apple1 = "apple";
         String apple2 = "apple";
         String apple3 = "appleapple";
+        String apple4 = new String(apple1);
 
-        System.out.println(System.identityHashCode(apple3)); //359023572
+        // String은 값 그 자체를 비교하자
+        System.out.println(apple1.equals(apple2));
+        System.out.println(apple1.equals(apple4));
+        System.out.println((apple1 + apple2).equals(apple3));
+
+/*        System.out.println(System.identityHashCode(apple3)); //359023572
         System.out.println(System.identityHashCode("apple" + "apple")); // 359023572
-        System.out.println(System.identityHashCode(apple1+ apple2)); // 66557614
+        System.out.println(System.identityHashCode(apple1+ apple2)); // 66557614*/
     }
 }
